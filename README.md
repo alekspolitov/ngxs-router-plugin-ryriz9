dad444f96f6077fb069de48a9b03e6f17c8d2e77
alekspolitov <aleks.politov@gmail.com>
2021-12-18 22:41:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngxs-router-plugin-ryriz9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngxs-router-plugin-ryriz9)
---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Actions, ofActionSuccessful } from '@ngxs/store';
+import { RouterNavigation } from '@ngxs/router-plugin';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent implements OnInit {
+  constructor(
+    private actions$: Actions
+  ) {}
+
+  ngOnInit() {
+    this.actions$.pipe(
+      ofActionSuccessful(RouterNavigation)
+    ).subscribe(() => {
+      console.warn('::: RouterNavigation Plugin Event :::');
+    });
+  }
+}
